6231450d10b375f2d0d11cdb0b2ba198614d80e7
magicono43 <kirkoliveri@gmail.com>
2023-10-23 21:21:18 -0400

    Made Massive Save-File Space Saving Improvements, 10/23/2023
    Made Massive Save-File Space Saving Improvements. So after using a method of "manual compression" of the array data into a compressed string representation, then when loading a save translating/decompressing that back into the same array data. The initial ~6MB mod save-data went all the way down to about 4.2 KB, atleast in this case a nearly 1400 times difference! So I'm very happy with this compression method. Sure it won't work in ever instance, but for this time it is a massive difference and appears to work perfectly fine compressing it down and decompressing it back to the original array values. So for now atleast, that's probably all the save-data I'm going to work with and I'm going to probably try working on some other features again, now that I have that basic framework layed down atleast, which I should hopefully be able to fairly easily add onto when needed. But yeah, next time I'll maybe work on the Line of sight stuff maybe? That being changing the "exploration/perception" radius based on what terrain elevation you are standing on and the elevation values nearby, will see, but great progress today, 10/23/2023.

--- Scripts/OOTMain.cs
+++ Scripts/OOTMain.cs
@@ -3,7 +3,7 @@
 // License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
 // Author:          Kirk.O
 // Created On: 	    8/3/2023, 8:40 PM
-// Last Edit:		10/22/2023, 9:50 PM
+// Last Edit:		10/23/2023, 9:30 PM
 // Version:			1.00
 // Special Thanks:  
 // Modifier:
--- Scripts/OOTSaveData.cs
+++ Scripts/OOTSaveData.cs
@@ -1,12 +1,14 @@
 using DaggerfallWorkshop.Game.Utility.ModSupport;
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace OverhauledOverworldTravel
 {
     [FullSerializer.fsObject("v1")]
     public class OOTSaveData : IHasModSaveData
     {
-        public int[] ExploredPixelsSaved;
+        public string ExploredPixelsCompressedSaved;
 
         public Type SaveDataType
         {
@@ -16,13 +18,7 @@ namespace OverhauledOverworldTravel
         public object NewSaveData()
         {
             OOTSaveData emptyData = new OOTSaveData();
-            emptyData.ExploredPixelsSaved = new int[1000 * 500];
-
-            // Fill "fog of war" tracking array with empty values initially
-            for (int i = 0; i < emptyData.ExploredPixelsSaved.Length; i++)
-            {
-                emptyData.ExploredPixelsSaved[i] = 0; // Not sure if this filling with "0"s is entirely necessary, but doing it anyway, just in case.
-            }
+            emptyData.ExploredPixelsCompressedSaved = "a500000x0"; // Text representation of a 0'ed out array with 500,000 values, or "empty."
 
             return emptyData;
         }
@@ -44,14 +40,103 @@ namespace OverhauledOverworldTravel
                 }
             }
 
-            data.ExploredPixelsSaved = (int[])arrayData1D.Clone(); // DFU's save-load framework does not support multi-dimensional arrays, so have to convert to a 1D array.
+            // Start converting 1D array data into "compressed" string to be used in save-data.
+            List<string> compressedData = new List<string>();
+            int currentCount = 0;
+            int currentValue = -1; // Initialize to a value not in your data set
+
+            foreach (int value in arrayData1D)
+            {
+                if (value != currentValue)
+                {
+                    // A new value is encountered, add the marker
+                    if (currentCount > 0)
+                    {
+                        compressedData.Add($"a{currentCount}x{currentValue}");
+                    }
+
+                    // Update current value and count
+                    currentValue = value;
+                    currentCount = 1;
+                }
+                else
+                {
+                    // Value is the same as the previous, increment the count
+                    currentCount++;
+                }
+            }
+
+            // Add the last marker
+            if (currentCount > 0)
+            {
+                compressedData.Add($"a{currentCount}x{currentValue}");
+            }
+
+            // Join the compressed data into a single string
+            string compressedString = string.Join("", compressedData);
+
+            data.ExploredPixelsCompressedSaved = compressedString; // DFU's save-load framework does not support multi-dimensional arrays, so have to convert to a 1D array.
             return data;
         }
 
         public void RestoreSaveData(object dataIn)
         {
             OOTSaveData data = (OOTSaveData)dataIn;
-            int[] arrayData1D = (int[])data.ExploredPixelsSaved.Clone();
+            string compressedString = data.ExploredPixelsCompressedSaved;
+            List<int> dataArray = new List<int>();
+
+            // Translate/Decompress save-data from a string to a 1D array to be used again by the game as a proper variable.
+            int i = 0;
+            while (i < compressedString.Length)
+            {
+                // Check for the "a" marker
+                if (compressedString[i] == 'a')
+                {
+                    // Find the end of the counting value
+                    int j = i + 1;
+                    while (j < compressedString.Length && compressedString[j] != 'x')
+                    {
+                        j++;
+                    }
+
+                    // Extract the counting value
+                    if (j < compressedString.Length)
+                    {
+                        int count = int.Parse(compressedString.Substring(i + 1, j - i - 1));
+
+                        // Find the end of the value
+                        int k = j + 1;
+                        while (k < compressedString.Length && compressedString[k] != 'a')
+                        {
+                            k++;
+                        }
+
+                        // Extract the value
+                        if (k <= compressedString.Length)
+                        {
+                            int value = int.Parse(compressedString.Substring(j + 1, k - j - 1));
+
+                            // Add the value to the array count times
+                            for (int l = 0; l < count; l++)
+                            {
+                                dataArray.Add(value);
+                            }
+
+                            i = k;
+                        }
+                    }
+                }
+                else
+                {
+                    // If no marker is found, something might be wrong with the input string, handle this error condition appropriately
+                    Debug.LogError("Something Terrible Happened Loading OOT's Save Data, Missed The Letter (a)!");
+                    break;
+                }
+            }
+
+            // Convert the List<int> to a 1D array
+            int[] arrayData1D = dataArray.ToArray();
+
             int[,] arrayData2D = new int[1000, 500];
 
             // Convert 1D array to 2D array to be used as data for the "ExploredPixelValues" variable in the OOTMain class
